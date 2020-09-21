3d3611e259049ad7983c59a1ce47f8fed3ce00ce
sacha-smz <sacha.zacaropoulos@gmail.com>
2020-09-21 16:35:00 +0200

    chore: react app setup

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <header>
+        <span className="title">Memorella</span>Le memory de Stella
+      </header>
+
+      <main></main>
+    </>
+  );
+}
+
+export default App;
